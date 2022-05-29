d5dbe441071899099c79180e15b3bf0ccb143b0d
Rumen Neshev <rstar2@abv.bg>
2022-05-29 14:37:45 +0300

    asd

--- apps/webcam/src/webcapture-image.sh
+++ apps/webcam/src/webcapture-image.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+RESOLUTION_DEF=1920
+RESOLUTION=${2:-$RESOLUTION_DEF}
+
 # take just one image
 # ffmpeg -f v4l2 -i /dev/video0 -f singlejpeg -frames:v 1 -qscale:v $1
 
 # use fswebcam  (delay 2 secs and skip first 2 frames) - save it as the passed 1-st argument
 # fswebcam --device v4l2:/dev/video0 --delay 2 --skip 2 --frames 1 ./public/history/image.jpg
 # NOTE: The output image is overwritten (this is useful in this case)
-fswebcam --device v4l2:/dev/video0 --delay 2 --skip 2 --frames 1 $1
+fswebcam --device v4l2:/dev/video0 --delay 2 --skip 2 --frames 1 --resolution $RESOLUTION $1
 
 
 # for knowage: The  PNG or JPEG image can be sent to stdio using the filename "-".
